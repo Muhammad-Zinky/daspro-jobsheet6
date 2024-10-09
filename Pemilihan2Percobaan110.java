c9dc3569eb862cc291f4e1c55c3397be1a63d963
mohammad zinky <mohammadzinky@gmail.com>
2024-10-09 18:54:24 +0700

    jobsheet 6

---
+++ Pemilihan2Percobaan110.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+    public class Pemilihan2Percobaan110{
+        public static void main(String[] args){
+
+            Scanner input10 = new Scanner(System.in);
+
+            System.out.print("Masukkan Tahun : ");
+            int Tahun = input10.nextInt();
+
+            if ((Tahun % 4) == 0) {
+                if ((Tahun % 100) != 0 || Tahun % 400 == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }   
+
+            } else {
+                System.out.println(Tahun + "Bukan Tahun Kabisat");
+            }
+        }
+    }
